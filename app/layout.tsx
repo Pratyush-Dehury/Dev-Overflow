9a5867bd6b11b71cbf141212e61795b08eea611c
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2024-06-22 16:03:58 +0530

    Changed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,30 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevOverflow",
+  description:
+    "A community driven platform for developers to share and learn. Ask questions, share knowledge, and grow together.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -16,7 +39,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
