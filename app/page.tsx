07805c0a3e8433d6c988dcde3f6b718c10ea05d1
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2024-06-22 15:23:44 +0530

    Add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">DevOverflow</h1>
-      <h2 className="h2-bold">By, Next.js 14, I&apos;m coming</h2>
-      <h3 className="h3-bold">By Pratyush</h3>
-    </div>
-  );
-}
